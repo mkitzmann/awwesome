7ab419936dd6632b4f73aa5febcd94cc246f7779
mkitzmann <mkitzmann@gmail.com>
2023-08-03 21:17:31 +0200

    create query.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,41 +1,9 @@
-import db from '../selfhosted-db.json';
 import { TOKEN_GITHUB } from '$env/static/private';
-
-const searchString =
-	'repo:' + db.projects.map((project) => project.source_url.slice(19)).join(' repo:');
+import { getQuery } from './query';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-	const query = `
-	query {
-	  search(
-		type:REPOSITORY,
-		query: "${searchString}",
-		last: 100
-	  ) {
-		repos: edges {
-		  repo: node {
-			... on Repository {
-			  url
-			  description
-			  name
-
-
-			  stargazers {
-				totalCount
-			  }
-			  allIssues: issues {
-				totalCount
-			  }
-			  openIssues: issues(states:OPEN) {
-				totalCount
-			  }
-			}
-		  }
-		}
-	  }
-	}
-	`;
+	const query = getQuery();
 	try {
 		const response = await fetch('https://api.github.com/graphql', {
 			method: 'POST',
---
+++ src/routes/query.ts
@@ -0,0 +1,38 @@
+import db from '../selfhosted-db.json';
+
+/** @type {import('./$types').PageServerLoad} */
+export function getQuery() {
+	const searchString =
+		'repo:' + db.projects.map((project) => project.source_url.slice(19)).join(' repo:');
+
+	return `
+	query {
+	  search(
+		type:REPOSITORY,
+		query: "${searchString}",
+		last: 100
+	  ) {
+		repos: edges {
+		  repo: node {
+			... on Repository {
+			  url
+			  description
+			  name
+
+
+			  stargazers {
+				totalCount
+			  }
+			  allIssues: issues {
+				totalCount
+			  }
+			  openIssues: issues(states:OPEN) {
+				totalCount
+			  }
+			}
+		  }
+		}
+	  }
+	}
+	`;
+}
