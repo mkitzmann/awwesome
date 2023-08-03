80b8f139e968fcf5c8e44b3a9fed4d8a5daf092f
mkitzmann <mkitzmann@gmail.com>
2023-08-03 22:59:28 +0200

    add all category

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,34 @@
 <script lang="ts">
 	import star from '$lib/assets/star.svg';
-	import type {ProjectCollection} from "../lib/types/types";
+	import type { ProjectCollection } from '../lib/types/types';
 	export let data: ProjectCollection;
 
-	let selectedCategory
-	$: categories = [...new Set(Object.values(data).map(project => project.category))]
-	$: projects = Object.values(data).filter(project => selectedCategory ? project.category === selectedCategory : true)
+	let allCategory = 'All';
+	let selectedCategory;
+	$: categories = [allCategory].concat([
+		...new Set(Object.values(data).map((project) => project.category))
+	]);
+	$: projects = Object.values(data)
+		.filter((project) => {
+			if (selectedCategory === allCategory) {
+				return true;
+			}
+			return selectedCategory ? project.category === selectedCategory : true;
+		})
+		.sort((a, b) => (a.stars < b.stars ? 1 : 0));
 </script>
 
-
 <div class="flex flex-col gap-4 max-w-4xl mx-auto my-8 p-4">
-	<div class="flex">
-	{#each categories as category}
-		<div class="text-sm bg-gray-100 rounded-full inline px-2 py-1">{category}</div>
-	{/each}
+	<div class="flex gap-2">
+		{#each categories as category}
+			<button
+				class="text-sm rounded-full inline px-2 py-1 {selectedCategory === category
+					? 'bg-gray-200'
+					: 'bg-gray-100'}"
+				on:click={() => (selectedCategory = category)}
+				>{category}
+			</button>
+		{/each}
 	</div>
 	{#each projects as project}
 		<article class="bg-white p-4 rounded-xl flex flex-col gap-4">
@@ -22,8 +37,7 @@
 			</a>
 			<div>{project.description}</div>
 			<div class="flex">
-
-			<div class="text-sm bg-gray-100 rounded-full inline px-2 py-1">{project.category}</div>
+				<div class="text-sm bg-gray-100 rounded-full inline px-2 py-1">{project.category}</div>
 			</div>
 			<div class="flex items-center gap-2 text-yellow-600">
 				<img src={star} alt="star" class="h-6" />{project.stars}
