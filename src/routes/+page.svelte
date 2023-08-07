667f7e08f33468a3c6b45cefd82824582edc6c23
mkitzmann <mkitzmann@gmail.com>
2023-08-07 13:44:07 +0200

    add category headline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import githubMark from '$lib/assets/github-mark.svg';
 	import type { ProjectCollection } from '../lib/types/types';
 	export let data: ProjectCollection;
+	// import slugify from '@sindresorhus/slugify';
 
 	let allCategory = 'All';
 	let selectedCategory;
@@ -15,11 +16,23 @@
 		}
 		return selectedCategory ? project.category === selectedCategory : true;
 	});
+
+	const setCategory = (category) => {
+		selectedCategory === category
+			? (selectedCategory = allCategory)
+			: (selectedCategory = category);
+	};
+	// const slugCategory = (input) => {
+	// 	if (!input) {
+	// 		return '';
+	// 	}
+	// 	return slugify(input.toString());
+	// };
 </script>
 
 <div class="flex flex-col gap-4 mx-auto my-8 p-4">
 	<div class="flex justify-between">
-		<h1 class="text-3xl font-bold mb-4">Selfhosted DB</h1>
+		<h1 class="text-3xl font-bold mb-4">Awwesome Selfhosted</h1>
 		<a href="https://github.com/mkitzmann/selfhosted-db"
 			><img src={githubMark} alt="Github repo" class="h-8" /></a
 		>
@@ -33,18 +46,21 @@
 						category
 							? 'bg-gray-200'
 							: ''}"
-						on:click={() =>
-							selectedCategory === category
-								? (selectedCategory = allCategory)
-								: (selectedCategory = category)}
-						>{category}
+						on:click={setCategory(category)}
+					>
+						{category}
 					</button>
 				{/each}
 			</div>
 		</div>
 		<div>
-			<div class="text-sm mb-4 text-right">
-				Count: {projects.length}
+			<div>
+				{#if selectedCategory}
+					<h2 class="font-bold text-xl">{selectedCategory}</h2>
+				{/if}
+				<div class="text-sm mb-4 text-right">
+					Count: {projects.length}
+				</div>
 			</div>
 			<div class="grid lg:grid-cols-2 2xl:grid-cols-3 gap-4">
 				{#each projects as project}
