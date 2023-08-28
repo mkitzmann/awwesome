dddeae9696d4dc0a385eaf40cedfaffe50ca3d50
mkitzmann <mkitzmann@gmail.com>
2023-08-28 14:16:46 +0200

    add Docker support

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,6 @@
 
 <SvelteSeo
 		title="Awwesome Selfhosted"
-		description="Find the most awesome open-source, self-hostable projects on the web. Projects are crawled from Awesome Selfhosted on Github."
+		description="Find the most awesome open-source, self-hostable projects on the web. Original data by the awesome-selfhosted community, licensed under CC-BY-SA 3.0."
 />
 <slot />
